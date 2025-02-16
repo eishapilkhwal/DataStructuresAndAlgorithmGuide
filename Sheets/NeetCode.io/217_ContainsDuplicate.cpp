aa08c0888e78862524895f5b121f8641dfec9ba1
Eisha Pilkhwal <105778322+eishapilkhwal@users.noreply.github.com>
2025-02-16 16:11:16 +0530

    Update 217_ContainsDuplicate.cpp
    Array Hashing

--- Sheets/NeetCode.io/217_ContainsDuplicate.cpp
+++ Sheets/NeetCode.io/217_ContainsDuplicate.cpp
@@ -1,1 +1,18 @@
+// https://leetcode.com/problems/contains-duplicate/description/
 
+// Solution 1 : 
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        vector<int> frequency (nums.size() + 1, 0) ;
+        for(int i = 0; i < nums.size(); i++){
+            frequency[nums[i]]++;
+
+            if (frequency[nums[i]] > 1){
+                return true;
+            }
+        }
+        return false;
+    }
+};
