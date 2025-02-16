c9378c0e9438310b15b5171de9979c8e8a57d60e
Eisha Pilkhwal <105778322+eishapilkhwal@users.noreply.github.com>
2025-02-16 16:13:51 +0530

    Update 217_ContainsDuplicate.cpp
    Accepted Solution added

--- Sheets/NeetCode.io/217_ContainsDuplicate.cpp
+++ Sheets/NeetCode.io/217_ContainsDuplicate.cpp
@@ -1,11 +1,28 @@
 // https://leetcode.com/problems/contains-duplicate/description/
 
-// Solution 1 : 
+// Solution 1 : -- Runtime error : Issue: Heap Buffer Overflow
 
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         vector<int> frequency (nums.size() + 1, 0) ;
+        for(int i = 0; i < nums.size(); i++){
+            frequency[nums[i]]++;
+
+            if (frequency[nums[i]] > 1){
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+// Solution 2 : -- Accepted
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> frequency;
         for(int i = 0; i < nums.size(); i++){
             frequency[nums[i]]++;
 
