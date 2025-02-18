417e13b9b4ebd68351e956e393a33fcc8a5b0c48
Eisha Pilkhwal <105778322+eishapilkhwal@users.noreply.github.com>
2025-02-18 08:52:14 +0530

    Create 1_TwoSum.cpp

---
+++ Sheets/NeetCode.io/1_TwoSum.cpp
@@ -0,0 +1,22 @@
+// 1. Two Sum
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> map;
+        for(int i = 0; i < nums.size(); i++){
+            int a = nums[i];
+            int more = target - a;
+            if (map.find(more) != map.end()){
+                return {map[more], i};
+            }
+            map[a] = i;
+            
+        }
+        return {-1, -1};
+    }
+};
+
+// Time Complexity : O(n)
+// Space Complexity : O(n)
