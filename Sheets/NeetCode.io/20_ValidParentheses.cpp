76c4ccb35072075458f4a025e4912692de486fb6
Eisha Pilkhwal <105778322+eishapilkhwal@users.noreply.github.com>
2025-02-26 19:00:10 +0530

    Create 20_ValidParentheses.cpp

---
+++ Sheets/NeetCode.io/20_ValidParentheses.cpp
@@ -0,0 +1,31 @@
+// 20. Valid Parentheses
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+
+        for(int i = 0; i < s.length(); i++){
+            if (s[i] == '{' || s[i] == '[' || s[i] == '('){
+                st.push(s[i]);
+            }
+            else{
+                if (st.empty())
+                    return false;
+
+                if ((s[i] == ']' && st.top() == '[') ||
+                (s[i] == '}' && st.top() == '{') ||
+                (s[i] == ')' && st.top() == '(')){
+                    st.pop();
+                }
+                else
+                    return false;
+            }
+        }
+
+        return st.empty();
+    }
+};
+
+// Time Complexity : o(n)
+// Space Complexity : O(n)
