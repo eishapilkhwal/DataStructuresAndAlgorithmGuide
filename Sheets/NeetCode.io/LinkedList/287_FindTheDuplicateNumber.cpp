11c6d015178fded3756c69fbade5c0fc4eec3ead
Eisha Pilkhwal <105778322+eishapilkhwal@users.noreply.github.com>
2025-03-12 16:59:28 +0530

    Create 287_FindTheDuplicateNumber.cpp

---
+++ Sheets/NeetCode.io/LinkedList/287_FindTheDuplicateNumber.cpp
@@ -0,0 +1,29 @@
+// 287. Find the Duplicate Number
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+
+        slow = nums[0];
+
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+};
+
+// Time Complexity : O(n)
+// Space Complexity : O(1)
