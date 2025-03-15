6cb362d02dda97a78c0b2e16e63cb922dd20f316
Eisha Pilkhwal <105778322+eishapilkhwal@users.noreply.github.com>
2025-03-15 15:15:03 +0530

    Update 145_BinaryTreePostorderTraversal.cpp

--- Tree_DS/145_BinaryTreePostorderTraversal.cpp
+++ Tree_DS/145_BinaryTreePostorderTraversal.cpp
@@ -47,3 +47,54 @@ public:
         return result;
     }
 };
+
+// Using Single Stack
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> result;
+        if (root == nullptr){
+            return result;
+        }
+        stack<TreeNode*> st;
+        TreeNode* node = root;
+        TreeNode* temp;
+        while(node != nullptr || !st.empty()){
+            if(node != nullptr){
+                st.push(node);
+                node = node->left;
+            }
+            else{
+                temp = st.top()->right;
+                if(temp == nullptr){
+                    temp = st.top();
+                    st.pop();
+                    result.push_back(temp->val);
+                    while(!st.empty() && temp == st.top()->right){
+                        temp = st.top();
+                        st.pop();
+                        result.push_back(temp->val);
+                    }
+                }
+                else{
+                    node = temp; 
+                }
+            }
+        }
+
+        return result;
+        
+    }
+};
