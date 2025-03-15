deed7aefcf4478c240241d108694025047d50fcc
Eisha Pilkhwal <105778322+eishapilkhwal@users.noreply.github.com>
2025-03-15 20:10:11 +0530

    Update 104_MaximumDepthOfBinaryTree.cpp

--- Tree_DS/104_MaximumDepthOfBinaryTree.cpp
+++ Tree_DS/104_MaximumDepthOfBinaryTree.cpp
@@ -1,5 +1,7 @@
 // 104. Maximum Depth of Binary Tree
 
+// Using DFS
+
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
@@ -14,3 +16,45 @@ public:
 };
 
 // Time Complexity : O(N)
+
+
+
+// Using BFS
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == nullptr){
+            return 0;
+        }
+        queue<TreeNode*> q;
+        int count = 0;
+        q.push(root);
+        while(!q.empty()){
+            int levelSize = q.size(); 
+            count++;
+            for(int i = 0; i < levelSize; i++){
+                TreeNode* node = q.front();
+                q.pop();
+                if(node->left != nullptr){
+                    q.push(node->left);
+                }
+                if(node->right != nullptr){
+                    q.push(node->right);
+                }
+            }
+        }
+        return count;
+    }
+};
