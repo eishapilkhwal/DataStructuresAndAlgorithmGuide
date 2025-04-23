c1506c64d41e065fbf877361d413c1da577442d8
Eisha Pilkhwal <105778322+eishapilkhwal@users.noreply.github.com>
2025-04-23 10:30:09 +0530

    Create 70_ClimbingStairs.cpp

---
+++ DynammicProgramming/70_ClimbingStairs.cpp
@@ -0,0 +1,21 @@
+// 70. Climbing Stairs
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        int prev2 = 1;
+        int prev = 1;
+
+        for(int i = 2; i <= n; i++){
+            int curr_i = prev2 + prev;
+            prev2 = prev;
+            prev = curr_i;
+        }
+
+        return prev;
+        
+    }
+};
+
+// O(N)
+// O(1)
