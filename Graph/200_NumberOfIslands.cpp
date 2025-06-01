a81eaafe4c936a95221efdcece3ee35d9a9d7c32
Eisha Pilkhwal <105778322+eishapilkhwal@users.noreply.github.com>
2025-06-01 14:50:33 +0530

    Create 200_NumberOfIslands.cpp

---
+++ Graph/200_NumberOfIslands.cpp
@@ -0,0 +1,45 @@
+// 200. Number of Islands
+
+
+// Using BFS by converting the adjacency matrix into adjacency list 
+
+class Solution {
+public:
+    void bfs(int node, vector<vector<int>>& adjLst, vector<int>& visited){
+        queue<int> q;
+        q.push(node);
+        visited[node] = 1;
+        while(!q.empty()){
+            int element = q.front();
+            q.pop();
+            for(auto it : adjLst[element]){
+                if(!visited[it]){
+                    q.push(it);
+                    visited[it] = 1;
+                }
+            }
+        }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+
+        vector<vector<int>> adjLst (n);
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(isConnected[i][j] == 1 && i != j){
+                    adjLst[i].push_back(j);
+                }
+            }
+        }
+        vector<int> visited(n, 0);
+       
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            if(!visited[i]){
+                count++;
+                bfs(i, adjLst, visited);
+            }
+        }
+        return count;
+    }
+};
